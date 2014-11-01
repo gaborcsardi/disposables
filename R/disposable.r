feded20bf24653154da233f1e20913700af94505
Gabor Csardi <csardi.gabor@gmail.com>
2014-11-01 15:14:59 -0400

    Disposing packages with dispose_packages()
    Also, better cleanup on error.

--- R/disposable.r
+++ R/disposable.r
@@ -79,6 +79,21 @@ with_libpath <- function(lib_path, ...) {
 
 #' Create, install, load and attach multiple disposable packages
 #'
+#' If a package with the same name as a disposable one, is
+#' loaded, then it will be unloaded. If a package with same name
+#' as a disposable on is installed in \code{lib_dir}, then
+#' it will be overwritten. (\code{lib_dir) is usually a temporary
+#' directory, so this is not a big problem.)
+#'
+#' @details
+#' Note that if you specify \code{lib_dir} and it points to an
+#' existing directory, \code{make_package} overwrites the packages
+#' there. If an error happens during installation or loading of
+#' the disposables packages, then it will \emph{not} restore the
+#' original contents of \code{lib_dir}, but it will remove
+#' all newly installed disposable packages, even the ones
+#' that were installed cleanly.
+#'
 #' @param ... Named expressions.
 #'   A separate package with the given name is created for each.
 #' @param lib_dir Directory to install the package to.
@@ -86,12 +101,17 @@ with_libpath <- function(lib_path, ...) {
 #'   deleted once the R session is over.
 #' @param imports The 'Imports' field in the DESCRIPTION file,
 #'   the packages to import in each disposable package.
-#' @return The \code{lib_dir}, invisibly.
+#' @return A named list with entries: \itemize{
+#'     \item \code{lib_dir} The directory in which the packages are
+#'       installed.
+#'     \item \code{package} The named of the packages.
+#'   }
 #'
 #' @export
+#' @seealso \code{\link{dispose_packages}}
 #' @examples
 #' \donttest{
-#' lib_dir <- make_packages(
+#' pkg <- make_packages(
 #'   foo1 = { f <- function() print("hello!") ; d <- 1:10 },
 #'   foo2 = { f <- function() print("hello again!") ; d <- 11:20 }
 #' )
@@ -99,25 +119,108 @@ with_libpath <- function(lib_path, ...) {
 #' foo2::f()
 #' foo1::d
 #' foo2::d
-#' unloadNamespace("foo1")
-#' unloadNamespace("foo2")
-#' unlink(lib_dir, recursive = TRUE)
+#' dispose_packages(pkg)
 #' }
 
 make_packages <- function(..., lib_dir = tempfile(),
                           imports = character()) {
-  if (!file.exists(lib_dir)) dir.create(lib_dir)
+
+  remove_lib_dir <- !file.exists(lib_dir)
+  if (remove_lib_dir) dir.create(lib_dir)
   exprs <- c(as.list(match.call(expand.dots = FALSE)$...))
+
+  pkgs <- list(lib_dir = lib_dir, packages = names(exprs))
+
+  ## Start clean
+  dispose_packages(pkgs, delete_lib_dir = FALSE)
+
+  ## Clean up on error
+  on.exit(dispose_packages(pkgs, delete_lib_dir = remove_lib_dir))
+
   for (i in seq_along(exprs)) {
     expr <- exprs[[i]]
     name <- names(exprs)[i]
     install_tmp_pkg(expr, pkg_name = name,
                      lib_dir = lib_dir, imports = imports)
-    ## Unload everything if an error happens
-    on.exit(try(unloadNamespace(name), silent = TRUE), add = TRUE)
     with_libpath(lib_dir, suppressMessages(library(name, quietly = TRUE,
                                                    character.only = TRUE)))
-    on.exit()
   }
-  invisible(lib_dir)
+
+  on.exit()
+  pkgs
+}
+
+#' Get rid of temporary packages
+#'
+#' @param packages A list returned by \code{\link{make_packages}}.
+#' @param unattach Whether to unattach the packages.
+#' @param unload Whether to unload the packages. It is not possible to
+#'   unload without unattaching.
+#' @param delete Whether to delete the installed packages from the
+#'   \code{lib_dir}. If \code{delete_lib_dir} is \code{TRUE}, then
+#'   this should be \code{TRUE} as well.
+#' @param delete_lib_dir Whether to delete the the whole \code{lib_dir}.
+#'
+#' @export
+#' @examples
+#' \donttest{
+#' pkg <- make_packages(
+#'   foo1 = { f <- function() print("hello!") ; d <- 1:10 },
+#'   foo2 = { f <- function() print("hello again!") ; d <- 11:20 }
+#' )
+#'
+#' foo1::f()
+#' foo2::f()
+#' foo1::d
+#' foo2::d
+#'
+#' ## Unattach only
+#' dispose_packages(pkg, unload = FALSE, delete = FALSE)
+#' "package:foo1" %in% search()
+#' "foo1" %in% loadedNamespaces()
+#' dir(pkg$lib_dir)
+#'
+#' ## Unload
+#' dispose_packages(pkg, delete = FALSE)
+#' "package:foo1" %in% search()
+#' "foo1" %in% loadedNamespaces()
+#' dir(pkg$lib_dir)
+#'
+#' ## Delete completely
+#' dispose_packages(pkg)
+#' "package:foo1" %in% search()
+#' "foo1" %in% loadedNamespaces()
+#' file.exists(pkg$lib_dir)
+#' }
+
+dispose_packages <- function(packages, unattach = TRUE, unload = unattach,
+                             delete = TRUE, delete_lib_dir = delete) {
+
+  if (!unattach && unload) stop("Cannot unload without unattaching")
+  if (!delete && delete_lib_dir) {
+    stop("Cannot delete lib_dir without deleting packages")
+  }
+
+  if (unattach) {
+    for (n in packages$packages) {
+      pn <- paste0("package:", n)
+      if (pn %in% search()) detach(pn, character.only = TRUE)
+    }
+  }
+
+  if (unload) {
+    for (n in packages$packages) {
+      if (n %in% loadedNamespaces()) unloadNamespace(n)
+    }
+  }
+
+  if (delete_lib_dir) {
+    unlink(packages$lib_dir, recursive = TRUE)
+  } else if (delete) {
+    for (n in packages$packages) {
+      unlink(file.path(packages$lib_dir, n), recursive = TRUE)
+    }
+  }
+
+  invisible()
 }
